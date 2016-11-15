6d30e904be5ca366e5d3a3ae653e3c759239a10a
Nathan Lanza <nathan@lanza.io>
2016-11-15 04:21:31 -0500

    add bare minimum statistics

---
+++ StatisticsChartsVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Charts
+
+
+class StatisticsChartsVC: UIViewController {
+    
+    
+}
+
+extension StatisticsChartsVC: ViewControllerFromStoryboard {
+    static var storyboardIdentifier: String { return "StatisticsChartsVC" }
+}
