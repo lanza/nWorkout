30d3466ac5dd9945ba6167c53f55ea43171f4361
Nathan Lanza <nathan@lanza.io>
2016-11-14 21:22:18 -0500

    start adding LiftTypeVC

---
+++ LiftTypeTVC.swift
@@ -0,0 +1,24 @@
+import UIKit
+import RxCocoa
+import RxSwift
+
+class LiftTypeTVC: UIViewController {
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    var liftTypes = UserDefaults.standard.value(forKey: "liftTypes") as? [String] ?? []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+   
+    func setupRx() {
+        Observable.just(liftTypes).bindTo(tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { index, string, cell in
+            cell.textLabel?.text = string
+        }.addDisposableTo(db)
+    }
+    
+    let db = DisposeBag()
+}
+
--- SelectWorkoutCoordinator.swift
+++ SelectWorkoutCoordinator.swift
@@ -1,13 +1,35 @@
 import UIKit
 import CoordinatorKit
+import RxSwift
+import RxCocoa
 
-class ActiveWorkoutCoordinator: Coordinator {
+class SelectWorkoutCoordinator: Coordinator {
     
-    var activeWorkout: Workout!
-    var workoutTVC: WorkoutTVC { return viewController as! WorkoutTVC }
+    var selectWorkoutVC: SelectWorkoutVC { return viewController as! SelectWorkoutVC }
     
     override func loadViewController() {
-        viewController = WorkoutTVC.new()
-        workoutTVC.workout = activeWorkout
+        viewController = SelectWorkoutVC.new()
     }
+    override func viewControllerDidLoad() {
+        super.viewControllerDidLoad()
+        
+        selectWorkoutVC.navigationItem.leftBarButtonItem!.rx.tap.subscribe(onNext: { [unowned self] in
+            self.navigationCoordinator?.parentCoordinator?.dismiss(animated: true)
+        }).addDisposableTo(db)
+        
+        selectWorkoutVC.view.setNeedsLayout()
+        
+        selectWorkoutVC.tableView.rx.modelSelected(Workout.self).subscribe(onNext: { routine in
+            let awc = self.didSelectRoutine(routine)
+            self.show(awc, sender: self)
+        }).addDisposableTo(db)
+        selectWorkoutVC.blankWorkoutButton.rx.tap.subscribe(onNext: {
+            let awc = self.didSelectRoutine(Workout())
+            self.show(awc, sender: self)
+        }).addDisposableTo(db)
+        
+    }
+    
+    var didSelectRoutine: ((Workout?) -> (ActiveWorkoutCoordinator))!
+    let db = DisposeBag()
 }
