c89d64e31b84985d79e1d724763413f2d98113de
Nathan Lanza <nathan@lanza.io>
2016-11-16 20:23:27 -0500

    Update gif

--- WorkoutTVC.swift
+++ WorkoutTVC.swift
@@ -12,6 +12,8 @@ class WorkoutTVC: UIViewController {
     var workout: Workout!
     var isActive: Bool { return !workout.isComplete }
     
+    var keyboardHandler: KeyboardHandler!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -26,6 +28,8 @@ class WorkoutTVC: UIViewController {
         tableView.tableFooterView = UIView()
         
         navigationItem.rightBarButtonItem = editButtonItem
+        
+        keyboardHandler = KeyboardHandler.new(tableView: tableView, view: view)
     }
     func addNewLift(name: String) {
         let lift = Lift()
@@ -62,5 +66,62 @@ extension WorkoutTVC: UITableViewDelegate {
     }
 }
 
+class KeyboardHandler: NSObject {
+    weak var tableView: UITableView!
+    weak var view: UIView!
+    static func new(tableView: UITableView, view: UIView) -> KeyboardHandler {
+        let kh = KeyboardHandler()
+        kh.tableView = tableView
+        kh.view = view
+        NotificationCenter.default.addObserver(kh, selector: #selector(KeyboardHandler.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(kh, selector: #selector(KeyboardHandler.keyboardDidShow(_: )), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(kh, selector: #selector(KeyboardHandler.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        return kh
+    }
+    var defaultInsets: UIEdgeInsets!
+    var keyboardHeight: CGFloat!
+    func keyboardWillShow(_ notification: Notification) {
+        if let userInfo = notification.userInfo {
+            defaultInsets = tableView.contentInset
+            let value = (userInfo[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+            keyboardHeight = value?.height ?? view.frame.height * CGFloat(Lets.keyboardToViewRatio)
+            
+            
+            let insets = UIEdgeInsets(top: defaultInsets.top, left: defaultInsets.left, bottom: keyboardHeight, right: defaultInsets.right)
+            tableView.contentInset = insets
+            tableView.scrollIndicatorInsets = insets
+        }
+    }
+    func keyboardDidShow(_ notification: Notification) {
+        scrollToTextField()
+    }
+    
+    func scrollToTextField() {
+        print(#function)
+        
+        if let firstResponder = UIResponder.currentFirstResponder() as? UIView {
+            let frFrame = firstResponder.frame
+            let corrected = UIApplication.shared.keyWindow!.convert(frFrame, from: firstResponder.superview)
+            let yRelativeToKeyboard = (view.frame.height - keyboardHeight) - (corrected.origin.y + corrected.height)
+            print(yRelativeToKeyboard)
+            if yRelativeToKeyboard < 0 {
+                print("what")
+                let frInViewsFrame = view.convert(frFrame, from: firstResponder.superview)
+                let scrollPoint = CGPoint(x: 0, y: frInViewsFrame.origin.y - keyboardHeight - tableView.contentInset.top - frInViewsFrame.height - UIApplication.shared.statusBarFrame.height)
+                tableView.setContentOffset(scrollPoint, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        UIView.animate(withDuration: 0.2) {
+            guard let defaultInsets = self.defaultInsets else { return }
+            self.tableView.contentInset = defaultInsets
+        }
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
 
 
