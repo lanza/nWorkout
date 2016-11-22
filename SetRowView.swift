39a47b6bbae878bc53d1fc1280e27cdd239abdc9
Nathan Lanza <nathan@lanza.io>
2016-11-21 20:53:00 -0500

    Scaling for fail button

--- SetRowView.swift
+++ SetRowView.swift
@@ -57,6 +57,10 @@ class FailButton: UIButton {
         super.init(frame: CGRect.zero)
         setTitle("F", for: UIControlState())
         setTitleColor(.black, for: UIControlState())
+        
+        titleLabel?.numberOfLines = 1
+        titleLabel?.minimumScaleFactor = 5/titleLabel!.font.pointSize
+        titleLabel?.adjustsFontSizeToFitWidth = true
     }
     required init?(coder aDecoder: NSCoder) { fatalError() }
 }
