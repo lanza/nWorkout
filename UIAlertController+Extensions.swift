aac79ee828ea951f76add226a9d8931e81c55ba1
Nathan Lanza <nathan@lanza.io>
2017-01-04 21:01:26 -0500

    Add static method to UIAlertController extension that creates a confirmation alert with a closure

--- UIAlertController+Extensions.swift
+++ UIAlertController+Extensions.swift
@@ -9,4 +9,11 @@ extension UIAlertController {
         }
         return a
     }
+    
+    static func confirmAction(title: String, message: String, onConfirm: @escaping (UIAlertAction) -> ()) -> UIAlertController {
+        let a = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        a.addAction(UIAlertAction(title: Lets.yes, style: .default, handler: onConfirm))
+        a.addAction(UIAlertAction(title: Lets.cancel, style: .cancel))
+        return a
+    }
 }
