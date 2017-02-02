316c5b9c29bf1fc49431d60fd7ad6f89068c1839
Nathan Lanza <nathan@lanza.io>
2017-02-02 00:41:32 -0500

    First try with new settings

--- SettingsTVC.swift
+++ SettingsTVC.swift
@@ -1,30 +1,71 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import RxDataSources
 
-extension SettingsTVC: ViewControllerFromStoryboard {
+enum SettingsSectionsModel {
+    case top
+    case cells(items: [String])
 }
+extension SettingsSectionsModel: SectionModelType {
+    typealias Item = String
+    var items: [String] {
+        switch self {
+        case .top:
+            return ["Hide completed reps and weight until the fail button was tapped?"]
+        case .cells(items: let strings):
+            return strings
+        }
+    }
+    init(original: SettingsSectionsModel, items: [String]) {
+        switch original {
+        case .top:
+            self = .top
+        case .cells(let strings):
+            self = .cells(items: strings)
+        }
+    }
+}
+extension SettingsSectionsModel {
+    
+}
+
 
 class SettingsTVC: UIViewController {
     
-    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+    let tableView = UITableView(frame: .zero, style: .grouped)
+    let viewInfos$ = Variable<[ViewInfo]>([])
     
-    @IBOutlet weak var hideCompletionUntilFailTappedSwitch: UISwitch!
-    
-    @IBOutlet weak var tableView: UITableView!
-
-    var viewInfos$ = Variable<[ViewInfo]>([])    
+    let dataSource = RxTableViewSectionedReloadDataSource<SettingsSectionsModel>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         viewInfos$.value = ViewInfo.saved
-        hideCompletionUntilFailTappedSwitch.isOn = ViewInfo.usesCombinedView
+        
+        setupTableView()
 
-        setupRx()
+//        setupRx()
 
         tableView.setEditing(true, animated: false)
-        tableView.isScrollEnabled = false
+        
+        
+    }
+    
+    func setupTableView() {
+        tableView.register(UITableViewCell.self)
+        let sections: [SettingsSectionsModel] = [.top, .cells(items: ["hi"])]
+        Observable.just(sections).bindTo(tableView.rx.items(dataSource: dataSource)).addDisposableTo(db)
+        tableView.rx.setDelegate(self)
+        
+        dataSource.configureCell = { ds, tv, ip, item in
+            let cell = tv.dequeueReusableCell(for: ip)
+            cell.textLabel?.text = item
+            return cell
+        }
+        dataSource.titleForFooterInSection = { ds, index in
+            return index == 0 ? "Settings" : "Cells"
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -33,54 +74,55 @@ class SettingsTVC: UIViewController {
         UserDefaults.standard.set(viewInfos$.value.map { $0.array }, forKey: Lets.viewInfoKey)
     }
     
-    func setupRx() {
-        
-        hideCompletionUntilFailTappedSwitch.rx.controlEvent(.valueChanged).subscribe(onNext: { [unowned self] in
-           
-            ViewInfo.setUsesCombinedView(self.hideCompletionUntilFailTappedSwitch.isOn)
-            
-            if self.hideCompletionUntilFailTappedSwitch.isOn {
-            
-                let cwIndex = self.viewInfos$.value.index { $0.name == Lets.completedWeightKey }!
-                self.viewInfos$.value[cwIndex].name = Lets.doneButtonCompletedWeightCompletedRepsKey
-                let crIndex = self.viewInfos$.value.index { $0.name == Lets.completedRepsKey }!
-                self.viewInfos$.value.remove(at: crIndex)
-                let dbIndex = self.viewInfos$.value.index { $0.name == Lets.doneButtonKey }!
-                self.viewInfos$.value.remove(at: dbIndex)
-                
-            } else {
-                
-                let value = self.viewInfos$.value
-                let cIndex = value.index { $0.name == Lets.doneButtonCompletedWeightCompletedRepsKey }!
-            
-                self.viewInfos$.value[cIndex].name = Lets.completedWeightKey
-                self.viewInfos$.value.append(contentsOf: [ViewInfo(name: Lets.completedRepsKey, width: 20, isOn: true), ViewInfo(name: Lets.doneButtonKey, width: 20, isOn: true)])
-            }
-        }).addDisposableTo(db)
-        
-        viewInfos$.asObservable().bindTo(tableView.rx.items(cellIdentifier: "cell", cellType: SettingsCell.self)) { index, viewInfo, cell in
-            cell.db = DisposeBag()
-            cell.nameLabel.text = viewInfo.name
-            cell.widthTextField.text = viewInfo.width.remainder(dividingBy: 1) == 0 ? String(describing: Int(viewInfo.width)) : String(describing: viewInfo.width)
-            cell.widthTextField.rx.controlEvent(.editingDidEnd).subscribe(onNext: { [unowned self] in
-                self.viewInfos$.value[index].width = CGFloat(Double(cell.widthTextField.text ?? "20")!)
-            }).addDisposableTo(cell.db)
-
-            
-            cell.isOnSwitch.isOn = viewInfo.isOn
-            cell.isOnSwitch.rx.controlEvent(.valueChanged).subscribe(onNext: { [unowned self] in
-                self.viewInfos$.value[index].isOn = cell.isOnSwitch.isOn
-            }).addDisposableTo(cell.db)
-            
-        }.addDisposableTo(db)
+//    
+//    func setupRx() {
     
-        tableView.rx.itemMoved.subscribe(onNext: { itemMovedEvent in
-            let item = self.viewInfos$.value.remove(at: itemMovedEvent.sourceIndex.row)
-            self.viewInfos$.value.insert(item, at: itemMovedEvent.destinationIndex.row)
-        }).addDisposableTo(db)
+//        hideCompletionUntilFailTappedSwitch.rx.controlEvent(.valueChanged).subscribe(onNext: { [unowned self] in
+//           
+//            ViewInfo.setUsesCombinedView(self.hideCompletionUntilFailTappedSwitch.isOn)
+//            
+//            if self.hideCompletionUntilFailTappedSwitch.isOn {
+//            
+//                let cwIndex = self.viewInfos$.value.index { $0.name == Lets.completedWeightKey }!
+//                self.viewInfos$.value[cwIndex].name = Lets.doneButtonCompletedWeightCompletedRepsKey
+//                let crIndex = self.viewInfos$.value.index { $0.name == Lets.completedRepsKey }!
+//                self.viewInfos$.value.remove(at: crIndex)
+//                let dbIndex = self.viewInfos$.value.index { $0.name == Lets.doneButtonKey }!
+//                self.viewInfos$.value.remove(at: dbIndex)
+//                
+//            } else {
+//                
+//                let value = self.viewInfos$.value
+//                let cIndex = value.index { $0.name == Lets.doneButtonCompletedWeightCompletedRepsKey }!
+//            
+//                self.viewInfos$.value[cIndex].name = Lets.completedWeightKey
+//                self.viewInfos$.value.append(contentsOf: [ViewInfo(name: Lets.completedRepsKey, width: 20, isOn: true), ViewInfo(name: Lets.doneButtonKey, width: 20, isOn: true)])
+//            }
+//        }).addDisposableTo(db)
         
-        tableView.rx.setDelegate(self).addDisposableTo(db)
-    }
+//        viewInfos$.asObservable().bindTo(tableView.rx.items(cellIdentifier: "cell", cellType: SettingsCell.self)) { index, viewInfo, cell in
+//            cell.db = DisposeBag()
+//            cell.nameLabel.text = viewInfo.name
+//            cell.widthTextField.text = viewInfo.width.remainder(dividingBy: 1) == 0 ? String(describing: Int(viewInfo.width)) : String(describing: viewInfo.width)
+//            cell.widthTextField.rx.controlEvent(.editingDidEnd).subscribe(onNext: { [unowned self] in
+//                self.viewInfos$.value[index].width = CGFloat(Double(cell.widthTextField.text ?? "20")!)
+//            }).addDisposableTo(cell.db)
+//
+//            
+//            cell.isOnSwitch.isOn = viewInfo.isOn
+//            cell.isOnSwitch.rx.controlEvent(.valueChanged).subscribe(onNext: { [unowned self] in
+//                self.viewInfos$.value[index].isOn = cell.isOnSwitch.isOn
+//            }).addDisposableTo(cell.db)
+//            
+//        }.addDisposableTo(db)
+//    
+//        tableView.rx.itemMoved.subscribe(onNext: { itemMovedEvent in
+//            let item = self.viewInfos$.value.remove(at: itemMovedEvent.sourceIndex.row)
+//            self.viewInfos$.value.insert(item, at: itemMovedEvent.destinationIndex.row)
+//        }).addDisposableTo(db)
+//        
+//        tableView.rx.setDelegate(self).addDisposableTo(db)
+//    }
     
     
     let db = DisposeBag()
