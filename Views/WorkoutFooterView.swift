2f00eca6683e3b9b63f2ead8aa790919fd3f126d
Nathan Lanza <nathan@lanza.io>
2017-02-03 00:23:41 -0500

    Update readme

--- Views/WorkoutFooterView.swift
+++ Views/WorkoutFooterView.swift
@@ -23,7 +23,7 @@ class WorkoutFooterView: UIView {
     let stackView = UIStackView(axis: .vertical, spacing: 4, distribution: .fillEqually)
     
     static func create(_ activeOrFinished: ActiveOrFinished) -> WorkoutFooterView {
-        let view = WorkoutFooterView(frame: CGRect(x: 0, y: 0, width: 0, height: activeOrFinished == .active ? (28*4+8*3) : 70  ))
+        let view = WorkoutFooterView(frame: CGRect(x: 0, y: 0, width: 0, height: activeOrFinished == .active ? (34*4 + 4*3 + 8*2) : (34*2 + 4*1 + 8*2)))
         view.activeOrFinished = activeOrFinished
         
         view.addSubview(view.stackView)
