488f87e070d4b4c42ff097df0133184f7cb9c60f
Nathan Lanza <nathan@lanza.io>
2020-02-26 21:26:11 -0800

    Clear some junk from AppDelegate

--- Coordinators/AppDelegate.swift
+++ Coordinators/AppDelegate.swift
@@ -3,18 +3,6 @@ import HealthKit
 import RealmSwift
 import UIKit
 
-//class Application {
-//    static func doOnFirstRun(closure: @escaping ()->()) {
-//        onFirstRunClosures.append(closure)
-//    }
-//    
-//    private static var onFirstRunClosures: [()->()] = []
-//  
-//    private static func callFirstRunClosures() {
-//        onFirstRunClosures.forEach { $0() }
-//    }
-//}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -29,36 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   let mainCoordinator = MainCoordinator()
 
-  func requestAccessToHealthKit() {
-    //        let healthStore = HKHealthStore()
-    //
-    //        let types = Swift.Set([HKObjectType.workoutType(), HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!])
-    //        healthStore.requestAuthorization(toShare: types, read: types) { (success, error) in
-    //            if !success {
-    //                print(error!)
-    //            }
-    //        }
-  }
-
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions:
       [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    requestAccessToHealthKit()
-
-    if let firstRun = UserDefaults.standard.value(forKey: "firstRun") as? Bool,
-      firstRun == false
-    {
-      //
-    } else {
-      UserDefaults.standard.set(
-        true,
-        forKey: Lets.combineFailAndCompletedWeightAndRepsKey
-      )
-      UserDefaults.standard.set(false, forKey: "firstRun")
-    }
 
     Realm.Configuration.defaultConfiguration = Realm.Configuration(
       schemaVersion: 2,
@@ -76,22 +39,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     for workout in workouts {
       let encoded = try! JSONEncoder().encode(workout)
       print(String(data: encoded, encoding: .utf8)!)
-    }
+    } 
 
-    // Not sure why I ever this chunk of code. Don't know when
-    // I would have wanted to delete everything
-    //        let lifts = try! Realm().objects(Lift.self)
-    //        for lift in lifts {
-    //            if lift.workout == nil {
-    //                lift.deleteSelf()
-    //            }
-    //        }
-    //        let sets = try! Realm().objects(Set.self)
-    //        for set in sets {
-    //            if set.lift == nil {
-    //                set.deleteSelf()
-    //            }
-    //        }
+//    let lifts = try! Realm().objects(Lift.self)
+//    for lift in lifts {
+//      if lift.workout == nil {
+//        lift.deleteSelf()
+//      }
+//    }
+//    let sets = try! Realm().objects(Set.self)
+//    for set in sets {
+//      if set.lift == nil {
+//        set.deleteSelf()
+//      }
+//    }
 
     window = UIWindow()
 
