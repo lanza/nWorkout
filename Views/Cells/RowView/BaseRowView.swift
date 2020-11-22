1fd0385ef3306253a140fd94fb376142126720ac
Nathan Lanza <nathan@lanza.io>
2020-11-21 22:20:27 -0500

    Remove some whitspace

--- Views/Cells/RowView/BaseRowView.swift
+++ Views/Cells/RowView/BaseRowView.swift
@@ -1,14 +1,11 @@
 import UIKit
 
 class BaseRowView: RowView {
-
   required init() {
     super.init()
-
   }
 
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
 }
