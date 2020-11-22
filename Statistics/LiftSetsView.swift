371585d211a069788da0cf1d2d9e64987f176007
Nathan Lanza <nathan@lanza.io>
2020-11-22 18:52:57 -0500

    Make individual sets better spaced and bordered

--- Statistics/LiftSetsView.swift
+++ Statistics/LiftSetsView.swift
@@ -3,21 +3,36 @@ import SwiftUI
 struct LiftSetsView: View {
   let lift: NLift
   var body: some View {
-    List {
+    LazyVStack(alignment: .center, spacing: 0) {
       ForEach(lift.sets) { set in
         HStack(alignment: .center) {
-          Text(String(set.weight))
           Spacer()
-          Text(String(set.reps))
+          HStack(alignment: .center) {
+            Text(String(set.weight))
+            Spacer()
+            Text(String(set.reps))
+            Spacer()
+            Text(String(set.completedWeight))
+            Spacer()
+            Text(String(set.completedReps))
+          }
+          .padding()
+          .border(Color.black)
           Spacer()
-          Text(String(set.completedWeight))
-          Spacer()
-          Text(String(set.completedReps))
         }
-        .listRowBackground(Color(Theme.Colors.dark))
-        //          .frame(height: CGFloat(30))
       }
     }
-    .frame(height: CGFloat(lift.sets.count * 44))
+    .frame(height: CGFloat(lift.sets.count * 60))
+  }
+}
+
+struct LiftSetsView_Previews: PreviewProvider {
+  static var previews: some View {
+    let s1 = NSet.makeDummy()
+    let s2 = NSet.makeDummy()
+    let l1 = NLift.makeDummy()
+    l1.append(s1)
+    l1.append(s2)
+    return LiftSetsView(lift: l1)
   }
 }
