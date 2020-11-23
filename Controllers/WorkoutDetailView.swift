3fd1902ef5a1384ff86d29bc494493e3393ba804
Nathan Lanza <nathan@lanza.io>
2020-11-22 19:55:28 -0500

    Clean up WorkoutDetailView a bit

--- Controllers/WorkoutDetailView.swift
+++ Controllers/WorkoutDetailView.swift
@@ -3,16 +3,39 @@ import SwiftUI
 struct WorkoutDetailView: View {
   @ObservedObject var workout: NWorkout
   var body: some View {
-    List {
+    LazyVStack {
       DatePicker(
         "Start Date", selection: $workout.startDate, in: ...Date(),
-        displayedComponents: [.date, .hourAndMinute])
+        displayedComponents: [.date, .hourAndMinute]
+      )
+      .foregroundColor(.white)
+      .padding()
+      .overlay(
+        RoundedRectangle(cornerRadius: 20)
+          .stroke(Color.black, lineWidth: 2)
+      )
       if workout.finishDate != nil {
         DatePicker(
           "Finish Date", selection: Binding($workout.finishDate)!,
-          in: ...Date(), displayedComponents: [.date, .hourAndMinute])
+          in: ...Date(), displayedComponents: [.date, .hourAndMinute]
+        )
+        .padding()
+        .foregroundColor(.white)
+        .overlay(
+          RoundedRectangle(cornerRadius: 20)
+            .stroke(Color.black, lineWidth: 2)
+        )
       }
       //      TextEditor(text: $workout.note)
     }
+    .padding()
+    Spacer(minLength: 50)
+  }
+}
+
+struct WorkoutDetail_Preview: PreviewProvider {
+  static var previews: some View {
+    let w = NWorkout.makeDummy()
+    return WorkoutDetailView(workout: w)
   }
 }
