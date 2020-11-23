ef7289816a5caea529f1ed27d4b347b6f4a96101
Nathan Lanza <nathan@lanza.io>
2020-11-22 19:55:42 -0500

    Remove the default cell approach that was accidentally added

--- DataSources/DataSource.swift
+++ DataSources/DataSource.swift
@@ -24,21 +24,12 @@ where Cell: ConfigurableCell, Provider.Object == Cell.Object {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
     -> Int
   {
-    if provider.numberOfItems() == 0 {
-      return 1
-    } else {
-      return provider.numberOfItems()
-    }
+    return provider.numberOfItems()
   }
-
-  let emptyCell = UITableViewCell()
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
     -> UITableViewCell
   {
-    if provider.numberOfItems() == 0 {
-      return emptyCell
-    }
     let cell =
       tableView.dequeueReusableCell(
         withIdentifier: Cell.reuseIdentifier,
