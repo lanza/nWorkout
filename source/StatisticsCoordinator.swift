b43995e4c8f7830569518897abcdab7adca0de35
Nathan Lanza <nathan@lanza.io>
2021-05-18 00:05:47 -0400

    Remove StatisticsTVC

--- source/StatisticsCoordinator.swift
+++ source/StatisticsCoordinator.swift
@@ -2,24 +2,10 @@ import SwiftUI
 import UIKit
 
 class StatisticsCoordinator: Coordinator {
-
-  var statisticsTVC: StatisticsTVC { return viewController as! StatisticsTVC }
-
   override func loadViewController() {
     let hostingVC = UIHostingController(
       rootView: StatisticsView(workouts: []))
     hostingVC.view.backgroundColor = Theme.Colors.darkest
     viewController = hostingVC
-    //    statisticsTVC.delegate = self
-  }
-}
-
-extension StatisticsCoordinator: StatisticsTVCDelegate {
-  func statisticsTVC(
-    _ statisticsTVC: StatisticsTVC,
-    didSelectLiftType liftType: String
-  ) {
-    let slsc = SingleLiftStatisticsCoordinator(liftName: liftType)
-    show(slsc, sender: self)
   }
 }
